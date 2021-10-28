2306b5b745c24d284e1e8a1c9a972a0f9c7c55dc
julianaaquinoo <juteixeira2012@gmail.com>
2021-10-28 18:04:11 -0300

    3 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { CartComponent } from './cart/cart.component';
     ReactiveFormsModule,
     RouterModule.forRoot([{ path: '', component: ProductListComponent },
     { path: 'products/:productId', component: ProductDetailsComponent },
+    { path: 'cart', component: CartComponent },
   ])
 ],
   
